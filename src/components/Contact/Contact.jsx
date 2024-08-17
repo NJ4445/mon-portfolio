d7543a4084e15c502007afa51918340826823077
MrRobot44100 <ndoumathejoy@gmail.com>
2024-08-17 14:30:41 +0200

    Modif formulaire de contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,6 +8,8 @@ function Contact() {
     message: ''
   });
 
+  const [confirmationMessage, setConfirmationMessage] = useState('');
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({
@@ -20,6 +22,16 @@ function Contact() {
     e.preventDefault();
     // Logique pour envoyer un email via un service comme EmailJS
     console.log('Form Submitted:', form);
+
+    // Réinitialiser le formulaire
+    setForm({
+      name: '',
+      email: '',
+      message: ''
+    });
+
+    // Afficher le message de confirmation
+    setConfirmationMessage('Your message has been sent successfully!');
   };
 
   return (
@@ -27,19 +39,47 @@ function Contact() {
       <h2>Contact</h2>
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.formGroup}>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" value={form.name} onChange={handleChange} required />
+          <label htmlFor="name"></label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={form.name}
+            onChange={handleChange}
+            placeholder="Name"
+            required
+          />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={form.email} onChange={handleChange} required />
+          <label htmlFor="email"></label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="Email"
+            required
+          />
         </div>
         <div className={styles.formGroup}>
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" value={form.message} onChange={handleChange} required></textarea>
+          <label htmlFor="message"></label>
+          <textarea
+            id="message"
+            name="message"
+            value={form.message}
+            onChange={handleChange}
+            placeholder="Message"
+            required
+          ></textarea>
         </div>
-        <button type="submit" className={styles.submitButton}>Send</button>
+        <button type="submit" className={styles.submitButton}>Contacter moi</button>
       </form>
+      {confirmationMessage && (
+        <div className={styles.confirmationMessage}>
+          {confirmationMessage}
+        </div>
+      )}
     </section>
   );
 }
