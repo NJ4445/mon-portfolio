5c4ffa50ea2d5acecc26420cd42c0acd75ce320a
MrRobot44100 <ndoumathejoy@gmail.com>
2024-08-18 12:14:37 +0200

    modif paragraph

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,7 +10,7 @@ function About() {
       <p>
         Je suis <strong>Joy Ndoumathe</strong>, un <strong>développeur web</strong> passionné par la création
         d’expériences web captivantes et interactives. Diplômée d'<strong>OpenClassrooms</strong> en 2024,
-        j'ai acquis des <strong>compétences solides</strong> pour offrir des <strong>solutions web innovantes</strong>
+        j'ai acquis des <strong>compétences solides</strong> pour offrir des <strong>solutions web innovantes</strong> 
         et optimisées. Mon parcours inclut une <strong>formation approfondie en développement web</strong>, axée sur
         l'<strong>intégration web</strong> et le <strong>développement front-end</strong>.
       </p>
